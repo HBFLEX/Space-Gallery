98f6e72a8d142db1f96ef0d92368c933be78da87
HBFLEX <happybanda@dyuni.ac.mw>
2024-07-16 21:12:02 +0200

    fix: webpack build

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+// Get the directory name from the file URL
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -8,7 +16,11 @@ const nextConfig = {
     },
     eslint: {
         ignoreDuringBuilds: true,
-    }, 
+    },
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.join(__dirname);
+        return config;
+    },
 };
 
 export default nextConfig;
